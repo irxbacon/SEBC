3a5e31d357858a6c24b9d3d4c4d687fb50e17a3d
Brian Cox <bcox@captechconsulting.com>
2016-09-22 08:31:15 -0400

    Corrected script persmissions

--- resources/tools/YARNtest.sh
+++ resources/tools/YARNtest.sh
@@ -1,3 +1,27 @@
+#!/bin/sh
+# Confirm the path values given below correspond to your installation
+
+########################################################
+#
+# Updated permissions in hdfs so the script could be run as my current user
+#
+#######################################################
+
+HADOOP_MR=/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce
+HADOOP_PATH=/opt/cloudera/parcels/CDH/bin
+
+# Mark start of the loop
+echo Testing loop started on `date`
+
+# Mapper containers
+for i in 2
+do
+   # Reducer containers
+   for j in 2
+   do
+      # Container memory
+      for k in 512
+      do
          # Set mapper JVM heap
          MAP_MB=`echo "($k*0.8)/1" | bc`
 
@@ -7,13 +31,13 @@
         echo "Maps $i , reducers $j, memory $k "      
 
         echo Generation 
-        time sudo -u hdfs $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
+        time $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      100000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err
         echo Sorting
-        time sudo -u hdfs $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
+        time $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.job.reduces=$j \
                      -Dmapreduce.map.memory.mb=$k \
@@ -23,8 +47,8 @@
                      /results/tg-10GB-${i}-${j}-${k}  \
                      /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err
 
-         sudo -u hdfs $HADOOP_PATH/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}
-         sudo -u hdfs $HADOOP_PATH/hadoop fs -rm -r -skipTrash /results/ts-10GB-${i}-${j}-${k}
+         $HADOOP_PATH/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}
+         $HADOOP_PATH/hadoop fs -rm -r -skipTrash /results/ts-10GB-${i}-${j}-${k}
       done
    done
 done
